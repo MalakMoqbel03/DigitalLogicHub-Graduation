dc0f24c5a68a6970796e67dda31a7d7bcdc3fd1b
MalakMoqbel03 <malakmoqbel18@gmail.com>
2026-06-19 23:03:09 +0300

    mm

--- frontend/my-app/src/services/api.js
+++ frontend/my-app/src/services/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const SESSION_KEY = "currentSession";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:8000",
   headers: {
     "Content-Type": "application/json",
   },
